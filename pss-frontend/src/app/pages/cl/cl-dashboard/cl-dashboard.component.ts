31630477a05df57c25d21e74e08c965cdc0a4ee2
Lawencon <lawencon@192.168.40.228>
2024-06-03 10:16:33 +0700

    added calendar in client

--- pss-frontend/src/app/pages/cl/cl-dashboard/cl-dashboard.component.ts
+++ pss-frontend/src/app/pages/cl/cl-dashboard/cl-dashboard.component.ts
@@ -1,18 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import interactionPlugin from '@fullcalendar/interaction';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-cl-dashboard',
   standalone: true,
   imports: [
     CommonModule,
-    NavbarComponent
+    NavbarComponent,
+    FullCalendarModule
   ],
   templateUrl: './cl-dashboard.component.html',
   styleUrl: './cl-dashboard.component.css'
 })
 export class ClDashboardComponent {
+  d: string = "2024-06-01"
 
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins:[dayGridPlugin, interactionPlugin],
+    // editable: true,
+    dateClick: this.handleDateClick.bind(this),
+    events: [
+      {title: "event 1", date: this.d}
+    ]
+  };
+
+  handleDateClick(arg: { dateStr: string; }) {
+    alert('Date clicked: ' + arg.dateStr);
+  }
 }
