cb42222c8c139db88bf2d1e2e89e6b267aebc04e
arthurlw <126365160+arthurlw@users.noreply.github.com>
2024-06-10 16:46:55 +0700

    Update navbar.component.ts

--- pss-frontend/src/app/pages/navbar/navbar.component.ts
+++ pss-frontend/src/app/pages/navbar/navbar.component.ts
@@ -4,7 +4,15 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { NotificationService } from '../../service/notification.service';
+import { EmailDto } from '../../dto/email/email.dto';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { NotificationResDto } from '../../dto/notification/notification-res.dto';
 
 @Component({
   selector: 'app-navbar',
@@ -14,9 +22,13 @@ import { MenubarModule } from 'primeng/menubar';
     MenubarModule,
     RouterModule,
     NavbarComponent,
+    ButtonModule,
+    OverlayPanelModule,
+    ToastModule
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  providers: [MessageService]
 })
 export class NavbarComponent implements OnInit {
   startItems: MenuItem[] = [];
@@ -26,7 +38,12 @@ export class NavbarComponent implements OnInit {
 
   userRole: string = "" ;
 
-  constructor(private userService: UserService, private authService: AuthService){}
+  emailDto: EmailDto = {email:localStorage.getItem('email') || ''}
+
+  result!: string;
+  messages!: string[];
+
+  constructor(private userService: UserService, private authService: AuthService, private messageService: MessageService, private notificationService: NotificationService, private fb:NonNullableFormBuilder ){}
   
   ngOnInit(): void {
     
@@ -36,7 +53,7 @@ export class NavbarComponent implements OnInit {
 
     this.endItems = [
       { label: 'Chat', icon: 'pi pi-inbox', routerLink: ['/chat'] },
-      { label: 'Notifications', icon: 'pi pi-bell', routerLink: ['/notifications'] },
+      { label: 'Notifications', icon: 'pi pi-bell', command: () => {this.showNotification("success","success","works"); }},
       {
         label: "Profile", icon:"pi pi-user", items: [
           { label: 'User Settings', icon: 'pi pi-cog', routerLink: ['/setting'], styleClass: 'ml-auto' },
@@ -66,13 +83,8 @@ export class NavbarComponent implements OnInit {
 
     const psItems: MenuItem[] = [
       ...commonItems,
-      { label: 'Client Schedules', icon: 'pi pi-calendar', routerLink: ['/calendar-list'] },
-      {
-        label: "Document", icon:"pi pi-file", items: [
-          { label: 'Upload', icon: 'pi pi-upload', routerLink: ['/Document/upload'] },
-          { label: 'Download', icon: 'pi pi-download', routerLink: ['/Document/download'] }
-        ]
-      },
+      { label: 'Client Schedules', icon: 'pi pi-calendar', routerLink: ['/PS/calendarview'] },
+      { label: 'Documents', icon: 'pi pi-file', routerLink: ['/client-calendar'] },
       { label: 'Create Client Schedule', icon: 'pi pi-calendar-plus', routerLink: ['/PS/createschedule'] },
       { label: 'Schedule Change Approval', icon: 'pi pi-check-circle', routerLink: ['/PS/schedulechangeapproval'] },
     ]
@@ -80,12 +92,6 @@ export class NavbarComponent implements OnInit {
     const clItems: MenuItem[] = [
       ...commonItems,
       { label: 'View Calendar', icon: 'pi pi-calendar', routerLink: ['/CL/viewcalendar'] },
-      {
-        label: "Document", icon:"pi pi-file", items: [
-          { label: 'Upload', icon: 'pi pi-upload', routerLink: ['/CL/document/upload'] },
-          { label: 'Download', icon: 'pi pi-download', routerLink: ['/CL/document/download'] }
-        ]
-      },
       { label: 'Request Change', icon: 'pi pi-send', routerLink: ['/CL/requestchange'] },
     ]
 
@@ -95,5 +101,12 @@ export class NavbarComponent implements OnInit {
     else return commonItems;
   }
 
-
+  showNotification(severity: string, summary: string, detail: string) {
+    this.notificationService.notificationsGetAll(this.emailDto).subscribe(res => {{
+      for(let i = 0; i < res.length; i++){
+          console.log(res[i])
+          this.messageService.add({ severity:"info", summary:"Notification", detail:res[i].message, life:9999999999 });
+      }
+    }})
+  }
 }
