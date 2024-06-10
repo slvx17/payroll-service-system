d1aa45dc72377d7087a806a6824a58f596c63400
arthurlw <126365160+arthurlw@users.noreply.github.com>
2024-06-10 17:02:46 +0700

    Update Notification.java

--- pss-backend/src/main/java/com/lawencon/pss_app/model/Notification.java
+++ pss-backend/src/main/java/com/lawencon/pss_app/model/Notification.java
@@ -1,7 +1,17 @@
 package com.lawencon.pss_app.model;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name="notifications")
