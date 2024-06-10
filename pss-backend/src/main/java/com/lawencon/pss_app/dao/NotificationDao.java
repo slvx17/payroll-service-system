ff16472f49d10bd0b4a0d36d67be8ee5b9819eb1
arthurlw <126365160+arthurlw@users.noreply.github.com>
2024-06-10 17:03:33 +0700

    Update NotificationDao.java

--- pss-backend/src/main/java/com/lawencon/pss_app/dao/NotificationDao.java
+++ pss-backend/src/main/java/com/lawencon/pss_app/dao/NotificationDao.java
@@ -1,5 +1,10 @@
 package com.lawencon.pss_app.dao;
 
+import java.util.List;
+
+import com.lawencon.pss_app.model.Notification;
+import com.lawencon.pss_app.model.User;
+
 public interface NotificationDao {
 
 	Notification create(Notification Notification);
