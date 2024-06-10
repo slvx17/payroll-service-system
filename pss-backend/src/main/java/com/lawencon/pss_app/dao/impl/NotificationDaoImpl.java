11e60f1224ca6fcefd4ecc16586ac6546f0cee3b
arthurlw <126365160+arthurlw@users.noreply.github.com>
2024-06-10 16:38:32 +0700

    Update NotificationDaoImpl.java

--- pss-backend/src/main/java/com/lawencon/pss_app/dao/impl/NotificationDaoImpl.java
+++ pss-backend/src/main/java/com/lawencon/pss_app/dao/impl/NotificationDaoImpl.java
@@ -3,5 +3,34 @@ package com.lawencon.pss_app.dao.impl;
 import com.lawencon.pss_app.dao.NotificationDao;
 
 public class NotificationDaoImpl implements NotificationDao{
+	private final NotificationRepo notificationRepo;
 
+	public NotificationDaoImpl(NotificationRepo notificationRepo) {
+		this.notificationRepo = notificationRepo;
+	}
+	
+	@Override
+	public Notification create(Notification notification) {
+		return notificationRepo.save(notification);
+	}
+
+	@Override
+	public Notification update(Notification notification) {
+		return notificationRepo.save(notification);
+	}
+
+	@Override
+	public List<Notification> findByReceiver(User user) {
+		return notificationRepo.findByReceiver(user);
+	}
+
+	@Override
+	public Notification findById(Long id) {
+		return notificationRepo.findById(id).orElse(null);
+	}
+
+	@Override
+	public void delete(Long id) {
+		notificationRepo.deleteById(id);
+	}
 }
