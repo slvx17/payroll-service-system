dc85cb92ff7b0bb5cc7b92e41c91fb9e457403d9
arthurlw <126365160+arthurlw@users.noreply.github.com>
2024-06-10 16:37:56 +0700

    Update NotificationDao.java

--- pss-backend/src/main/java/com/lawencon/pss_app/dao/NotificationDao.java
+++ pss-backend/src/main/java/com/lawencon/pss_app/dao/NotificationDao.java
@@ -2,4 +2,14 @@ package com.lawencon.pss_app.dao;
 
 public interface NotificationDao {
 
+	Notification create(Notification Notification);
+	
+	Notification update(Notification Notification);
+
+	List<Notification> findByReceiver(User user); // get all
+	
+	Notification findById(Long id);
+	
+	void delete(Long id);
+
 }
